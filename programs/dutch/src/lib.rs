aeabf697f7376e8e2361acf3dfa22e215eae5789
Mardiprk <mk1659770@gmail.com>
2026-01-16 05:14:09 +0000

    dutch

--- programs/dutch/src/lib.rs
+++ programs/dutch/src/lib.rs
@@ -16,8 +16,42 @@ pub mod dutch {
         end_time: i64,
         sell_amount: u64
     ) -> Result<()> {
-        let clock = Clock::get()?;
-        let rent = Rent::get()?;
+        // -- ChECKS --
+        require!(ctx.accounts.sell_mint.key() != ctx.accounts.buy_mint.key(), AuctionError::SameToken);
+        require!(start_price >= end_price, AuctionError::InvalidPrice);
+
+        let clock = Clock::get()?.unix_timestamp;
+
+        require!(clock <= start_time, AuctionError::InvalidTime);
+        require!(start_time < end_time, AuctionError::InvalidTime);
+        require!(sell_amount > 0, AuctionError::InvalidAmount);
+        
+        let cpi_accounts = Transfer{
+            from: ctx.accounts.seller_sell_ata.to_account_info(),
+            to: ctx.accounts.auction_sell_ata.to_account_info(),
+            authority: ctx.accounts.seller.to_account_info(),
+        };
+
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                cpi_accounts
+            ),
+            sell_amount
+        )?;
+
+        // -- STORE STATE -- 
+        let auction = &mut ctx.accounts.auction;
+
+        auction.seller = ctx.accounts.seller.key();
+        auction.sell_mint = ctx.accounts.sell_mint.key();
+        auction.buy_mint = ctx.accounts.buy_mint.key();
+        auction.start_price = start_price;
+        auction.end_price = end_price;
+        auction.start_time = start_time;
+        auction.end_time = end_time;
+        auction.sell_amount = sell_amount;
+        auction.bump = ctx.bumps.auction;
 
         Ok(())
 
@@ -27,6 +61,72 @@ pub mod dutch {
         ctx: Context<Buy>,
         max_price: u64,
     ) -> Result<()>{
+        let auction = &ctx.accounts.auction;
+        let now = Clock::get()?.unix_timestamp;
+
+        require!(now >= auction.start_time, AuctionError::NotStarted);
+        require!(now <= auction.end_time, AuctionError::Ended);
+
+        // ---- Linear price calculation ----
+        let elapsed = now - auction.start_time;
+        let duration = auction.end_time - auction.start_time;
+
+        let price_diff = auction.start_price - auction.end_price;
+        let decay = (price_diff as i128 * elapsed as i128) / duration as i128;
+
+        let current_price = auction.start_price - decay as u64;
+        
+        require!(current_price >= auction.end_price, AuctionError::InvalidPrice);
+        require!(current_price <= max_price, AuctionError::PriceTooHigh);
+
+        let total_cost = current_price.checked_mul(auction.sell_amount).ok_or(AuctionError::Overflow)?;
+        
+        //transfer buy token to seller
+        let cpi_accounts = Transfer{
+            from: ctx.accounts.buyer_buy_ata.to_account_info(),
+            to: ctx.accounts.seller_buy_ata.to_account_info(),
+            authority: ctx.accounts.buyer.to_account_info(),
+        };
+
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                cpi_accounts
+            ), 
+            total_cost
+        )?;
+
+        // transfer sell token to buyer (PDA Signer)
+        let seeds = &[b"auction", auction.seller.as_ref(),&[auction.bump]];
+
+        let cpi_accounts= Transfer{
+            from: ctx.accounts.auction_sell_ata.to_account_info(),
+            to: ctx.accounts.buyer_sell_ata.to_account_info(),
+            authority: ctx.accounts.auction.to_account_info(),
+        };
+
+        token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(), 
+                cpi_accounts,
+                &[seeds]
+            ),
+            auction.sell_amount
+        )?;
+
+        // -- CLOSE PDA ACcount
+        token::close_account(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                CloseAccount{
+                    account: ctx.accounts.auction_sell_ata.to_account_info(),
+                    destination: ctx.accounts.seller.to_account_info(),
+                    authority: ctx.accounts.auction.to_account_info(),
+                },
+                &[seeds]
+            )
+        )?;
+
 
         Ok(())
     }
@@ -86,6 +186,9 @@ pub struct Buy<'info> {
 
     #[account(mut)]
     pub seller_sell_ata: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub seller_buy_ata: Account<'info, TokenAccount>,
 
     #[account(mut)]
     pub buyer_buy_ata: Account<'info, TokenAccount>,
